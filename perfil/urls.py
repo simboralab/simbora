5253d44c64fa3a8a4e056a066813aade53b7c617
SidneyCirinoSF <sidneycirinosf@gmail.com>
2025-11-25 20:31:59 -0300

    Feat: módulo de recuperação de senha.

--- perfil/urls.py
+++ perfil/urls.py
@@ -6,13 +6,13 @@ from . import views
 # app_name = 'perfil'
 
 urlpatterns = [
-    path('password_change/', auth_views.PasswordChangeView.as_view(success_url='/perfil/password_change_done'), name='password_change'),
-    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(success_url='/perfil/password_reset_done'), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('account/', views.account_view,name='account'),
     path('sucesso/', views.sucess_view, name='sucesso'),
     path('logout/', views.logout_view, name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
