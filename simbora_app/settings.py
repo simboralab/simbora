d5a978b19f8ce82868311f35fd07da573ddd6e7a
SidneyCirinoSF <sidneycirinosf@gmail.com>
2025-12-01 20:15:49 -0300

    Feat: Adiciona html da main page e ajusta settings para envio de email com link para recuperacao de senha

--- simbora_app/settings.py
+++ simbora_app/settings.py
@@ -118,4 +118,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'suporte.simbora.app@gmail.com'
+EMAIL_HOST_PASSWORD = settings.SIMBORA_PASSWORD
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
