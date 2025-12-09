ff4af02bf27af5256b9d27d14e71a59420045b31
julia <meojulia1314@gmail.com>
2025-12-09 19:24:55 -0300

    Integração do template de visualizar evento

--- eventos/views.py
+++ eventos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Eventos
+from .models import Eventos, Participacao
 
 def lista_eventos(request):
     eventos = Eventos.objects.all().order_by("-data_inicio")
@@ -8,4 +8,37 @@ def lista_eventos(request):
 
 def visualizar_evento(request, evento_id):
     evento = get_object_or_404(Eventos, id=evento_id)
-    return render(request, "eventos/visualizar_evento.html", {"evento": evento})
+
+    confirmados = evento.participacoes.filter(status='CONFIRMADO')
+
+    total_confirmados = confirmados.count()
+
+    if evento.maximo_participantes:
+        try:
+            progresso = int((total_confirmados / evento.maximo_participantes) * 100)
+        except (TypeError, ZeroDivisionError):
+            progresso = 0
+        progresso = min(max(progresso, 0), 100)
+    else:
+        progresso = 0
+
+    if evento.maximo_participantes:
+        faltam = max(evento.maximo_participantes - total_confirmados, 0)
+    else:
+        faltam = None
+
+    esta_lotado = (
+        evento.maximo_participantes is not None and
+        total_confirmados >= evento.maximo_participantes
+    )
+
+    contexto = {
+        "evento": evento,
+        "confirmados": confirmados,        
+        "total_confirmados": total_confirmados,
+        "progresso": progresso,
+        "faltam": faltam,
+        "esta_lotado": esta_lotado,
+    }
+
+    return render(request, "eventos/visualizar_evento.html", contexto)
